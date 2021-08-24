ee9f58a707786a623d5657031a485587850f20cf
DarDog <samediacc666@gmial.com>
2021-08-24 21:19:27 +1100

    Update: index.js

--- pages/index.js
+++ pages/index.js
@@ -6,33 +6,23 @@ const background = document.querySelector('.body'),
 const handleButtonClick = () => {
   let firstColor = getColor(),
       secondColor = getColor();
-  changeBackgroundColor({
-    firstRgb: firstColor.rgb,
-    secondRgb: secondColor.rgb
-  })
-  changeHexColors({
-    firstHex: firstColor.hex,
-    secondHex: secondColor.hex
+  changeColors({
+    firstHex: firstColor,
+    secondHex: secondColor
   })
 }
 
 const getColor = () => {
-  let rgb = `rgb(`,
-      hex = ``
+  let hex = `#`
   for (let i = 0; i < 3; i++) {
     let int = getRandomInteger();
-    rgb = `${rgb}${int} `;
     hex = `${hex}${convertToHex(int)}`;
   }
-  rgb = `${rgb})`
-  return {
-    rgb: rgb,
-    hex: hex
-  }
+  return hex
 }
 
 const getRandomInteger = () => {
-  return Math.floor(Math.random()*255)
+  return Math.floor(Math.random() * 255)
 }
 
 const convertToHex = (int) => {
@@ -42,11 +32,8 @@ const convertToHex = (int) => {
   return `0${int.toString(16)}`
 }
 
-const changeBackgroundColor = ({ firstRgb, secondRgb }) => {
-  background.style.background = `linear-gradient(to right, ${firstRgb}, ${secondRgb}` ;
-}
-
-const changeHexColors = ({ firstHex, secondHex }) => {
+const changeColors = ({ firstHex, secondHex }) => {
+  background.style.background = `linear-gradient(to right, ${firstHex}, ${secondHex}`;
   firstHexCode.textContent = firstHex;
   secondHexCode.textContent = secondHex;
 }
